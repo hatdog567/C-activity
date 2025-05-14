4dddda7652189b13199cdef5cee3f628fd4fbde1
Hiraium56 <146530082+hatdog567@users.noreply.github.com>
2025-05-14 10:52:40 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Elementary_School
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxgrade = new System.Windows.Forms.TextBox();
+            this.grade1btn = new System.Windows.Forms.RadioButton();
+            this.grade2btn = new System.Windows.Forms.RadioButton();
+            this.grade3btn = new System.Windows.Forms.RadioButton();
+            this.grade4btn = new System.Windows.Forms.RadioButton();
+            this.grade5btn = new System.Windows.Forms.RadioButton();
+            this.grade6btn = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(280, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elementary School Level";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(280, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Roman Numeral: ";
+            // 
+            // txtboxgrade
+            // 
+            this.txtboxgrade.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxgrade.Location = new System.Drawing.Point(459, 329);
+            this.txtboxgrade.Name = "txtboxgrade";
+            this.txtboxgrade.Size = new System.Drawing.Size(100, 26);
+            this.txtboxgrade.TabIndex = 2;
+            // 
+            // grade1btn
+            // 
+            this.grade1btn.AutoSize = true;
+            this.grade1btn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade1btn.Location = new System.Drawing.Point(284, 135);
+            this.grade1btn.Name = "grade1btn";
+            this.grade1btn.Size = new System.Drawing.Size(83, 22);
+            this.grade1btn.TabIndex = 3;
+            this.grade1btn.TabStop = true;
+            this.grade1btn.Text = "Grade 1";
+            this.grade1btn.UseVisualStyleBackColor = true;
+            this.grade1btn.CheckedChanged += new System.EventHandler(this.grade1btn_CheckedChanged);
+            // 
+            // grade2btn
+            // 
+            this.grade2btn.AutoSize = true;
+            this.grade2btn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade2btn.Location = new System.Drawing.Point(284, 163);
+            this.grade2btn.Name = "grade2btn";
+            this.grade2btn.Size = new System.Drawing.Size(83, 22);
+            this.grade2btn.TabIndex = 4;
+            this.grade2btn.TabStop = true;
+            this.grade2btn.Text = "Grade 2";
+            this.grade2btn.UseVisualStyleBackColor = true;
+            this.grade2btn.CheckedChanged += new System.EventHandler(this.grade2btn_CheckedChanged);
+            // 
+            // grade3btn
+            // 
+            this.grade3btn.AutoSize = true;
+            this.grade3btn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade3btn.Location = new System.Drawing.Point(284, 191);
+            this.grade3btn.Name = "grade3btn";
+            this.grade3btn.Size = new System.Drawing.Size(83, 22);
+            this.grade3btn.TabIndex = 5;
+            this.grade3btn.TabStop = true;
+            this.grade3btn.Text = "Grade 3";
+            this.grade3btn.UseVisualStyleBackColor = true;
+            this.grade3btn.CheckedChanged += new System.EventHandler(this.grade3btn_CheckedChanged);
+            // 
+            // grade4btn
+            // 
+            this.grade4btn.AutoSize = true;
+            this.grade4btn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade4btn.Location = new System.Drawing.Point(284, 219);
+            this.grade4btn.Name = "grade4btn";
+            this.grade4btn.Size = new System.Drawing.Size(83, 22);
+            this.grade4btn.TabIndex = 6;
+            this.grade4btn.TabStop = true;
+            this.grade4btn.Text = "Grade 4";
+            this.grade4btn.UseVisualStyleBackColor = true;
+            this.grade4btn.CheckedChanged += new System.EventHandler(this.grade4btn_CheckedChanged);
+            // 
+            // grade5btn
+            // 
+            this.grade5btn.AutoSize = true;
+            this.grade5btn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade5btn.Location = new System.Drawing.Point(284, 247);
+            this.grade5btn.Name = "grade5btn";
+            this.grade5btn.Size = new System.Drawing.Size(83, 22);
+            this.grade5btn.TabIndex = 7;
+            this.grade5btn.TabStop = true;
+            this.grade5btn.Text = "Grade 5";
+            this.grade5btn.UseVisualStyleBackColor = true;
+            this.grade5btn.CheckedChanged += new System.EventHandler(this.grade5btn_CheckedChanged);
+            // 
+            // grade6btn
+            // 
+            this.grade6btn.AutoSize = true;
+            this.grade6btn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade6btn.Location = new System.Drawing.Point(284, 275);
+            this.grade6btn.Name = "grade6btn";
+            this.grade6btn.Size = new System.Drawing.Size(83, 22);
+            this.grade6btn.TabIndex = 8;
+            this.grade6btn.TabStop = true;
+            this.grade6btn.Text = "Grade 6";
+            this.grade6btn.UseVisualStyleBackColor = true;
+            this.grade6btn.CheckedChanged += new System.EventHandler(this.grade6btn_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grade6btn);
+            this.Controls.Add(this.grade5btn);
+            this.Controls.Add(this.grade4btn);
+            this.Controls.Add(this.grade3btn);
+            this.Controls.Add(this.grade2btn);
+            this.Controls.Add(this.grade1btn);
+            this.Controls.Add(this.txtboxgrade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxgrade;
+        private System.Windows.Forms.RadioButton grade1btn;
+        private System.Windows.Forms.RadioButton grade2btn;
+        private System.Windows.Forms.RadioButton grade3btn;
+        private System.Windows.Forms.RadioButton grade4btn;
+        private System.Windows.Forms.RadioButton grade5btn;
+        private System.Windows.Forms.RadioButton grade6btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elementary_School
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void grade1btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (grade1btn.Checked)
+                txtboxgrade.Text = "Grade I";
+        }
+
+        private void grade2btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (grade2btn.Checked)
+                txtboxgrade.Text = "Grade II";
+        }
+
+        private void grade3btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (grade3btn.Checked)
+                txtboxgrade.Text = "Grade III";
+        }
+
+        private void grade4btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (grade4btn.Checked)
+                txtboxgrade.Text = "Grade IV";
+        }
+
+        private void grade5btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (grade5btn.Checked)
+                txtboxgrade.Text = "Grade V";
+        }
+
+        private void grade6btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (grade6btn.Checked)
+                txtboxgrade.Text = "Grade VI";
+        }
+    }
+}
